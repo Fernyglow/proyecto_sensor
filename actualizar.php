38884ab20e510b9e0249ce72456c9a80ceb1aede
fernando <ferny2005vazquez@gmail.com>
2025-06-30 13:19:55 -0600

    hay vamos

---
+++ actualizar.php
@@ -0,0 +1,19 @@
+<?php
+include 'conexion.php';
+
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$id_area = $_POST['id_area'];
+
+$sql = "UPDATE usuarios 
+        SET nombre_user='$nombre', telefono='$telefono', area_id='$id_area' 
+        WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Usuario actualizado correctamente.<br>";
+  echo "<a href='mostrar.php'>Volver</a>";
+} else {
+  echo "Error al actualizar: " . $conn->error;
+}
+?>
